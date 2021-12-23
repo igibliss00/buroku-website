924b751fa3359b5598bf22379f90ab7e08104df0
igibliss00@gmail.com <igibliss00@gmail.com>
2021-12-23 11:55:50 -0500

    Minor info detail update

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Buroku`,
-    siteTitleAlt: `Buroku`,
-    siteHeadline: `Buroku`,
+    siteTitle: `Jongwoo Choi`,
+    siteTitleAlt: `Jongwoo Choi`,
+    siteHeadline: `Jongwoo Choi`,
     siteUrl: `https://buroku.gatsbyjs.io/`,
-    siteDescription: `Buroku is a software development company.`,
+    siteDescription: `Software Developer.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `Buroku`,
+    author: `Jongwoo Choi`,
   },
   plugins: [
     {
